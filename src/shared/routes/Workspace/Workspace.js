5d74957f189bf7b245a733714b07008185ef76ed
AdamTowers <adam.towers77@gmail.com>
2018-03-23 13:08:01 -0700

    question prompts using set html

--- src/shared/routes/Workspace/Workspace.js
+++ src/shared/routes/Workspace/Workspace.js
@@ -12,22 +12,48 @@ import FlatButton from '../../components/GeneralUI/FlatButton/FlatButton'
 
 // let prompt = "Suppose the following variables are defined: Indicate on each line below the output produced by each statement shown. If the statement produces more than one line of output indicate the line breaks with slashes as in a/b/c to indicate three lines of output with a followed by b followed by c. If the statement causes an error, write the word error to indicate this. "
 
-let prompt = `Write a method countMultiples that could be added to the IntTree class from lecture and section. The method returns a count of the number of multiples of a particular value in the binary tree. Given a number n, a value m is considered a multiple of n if it can be expressed as (k * n) for some integer k. For example, suppose that an IntTree variable tree stores a reference to the following tree:
+let prompt = `
+<p>Write a method countMultiples that could be added to the IntTree class from lecture and section. The method returns a count of the number of multiples of a particular value in the binary tree. Given a number n, a value m is considered a multiple of n if it can be expressed as (k * n) for some integer k. For example, suppose that an IntTree variable tree stores a reference to the following tree:</p>
 
-<br>The table below shows various calls and the values they should return:
-<br>Call 	Value Returned 	Reason
-<br>tree.countMultiples(2); 	6 	six multiples of 2 : 6, 2, 8, 6, 4, 0
-<br>tree.countMultiples(4); 	3 	three multiples of 4 : 8, 4, 0
-<br>tree.countMultiples(3); 	5 	five multiples of 3 : 6, 3, 6, 9, 0
-<br>tree.countMultiples(1); 	10 	all ten values are multiples of 1
+<p>The table below shows various calls and the values they should return:</p>
+<table >
+    <thead>
+      <th>Call</th>
+      <th>Value Returned</th>
+      <th>Reason</th>
+    </thead>
+	<tbody>
+		<tr>
+			<td>tree.countMultiples(2);</td>
+			<td>6</td>
+			<td>six multiples of 2 : 6, 2, 8, 6, 4, 0</td>
+		</tr>
+		<tr>
+			<td>tree.countMultiples(4);</td>
+			<td>3</td>
+			<td>three multiples of 4 : 8, 4, 0</td>
+		</tr>
+		<tr>
+			<td>tree.countMultiples(3);</td>
+			<td>5</td>
+			<td>five multiples of 3 : 6, 3, 6, 9, 0</td>
+		</tr>
+		<tr>
+			<td>tree.countMultiples(1);</td>
+			<td>10</td>
+			<td>all ten values are multiples of 1</td>
+		</tr>
+	</tbody>
+</table>
+<p>Your method should throw an <code>IllegalArgumentException</code> if passed the value 0.</p>
+<p>You may define private helper methods to solve this problem, but otherwise you may not call any other methods of the class nor create any data structures such as arrays, lists, etc. Your method should not change the structure or contents of the tree.</p>
+<p>Assume that you are adding this method to the IntTree class as defined below:</p>
+<!-- HTML generated using hilite.me --><div style="background: #272822; overflow:auto;width:auto;border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;"><pre style="margin: 0; line-height: 125%"><span style="color: #66d9ef">public</span> <span style="color: #66d9ef">class</span> <span style="color: #a6e22e">IntTree</span> <span style="color: #f92672">{</span>
+    <span style="color: #66d9ef">private</span> <span style="color: #f8f8f2">IntTreeNode</span> <span style="color: #f8f8f2">overallRoot</span><span style="color: #f92672">;</span>
+    <span style="color: #f92672">...</span>
+<span style="color: #f92672">}</span>
+</pre></div>
 
-<br>Your method should throw an IllegalArgumentException if passed the value 0.
-<br>You may define private helper methods to solve this problem, but otherwise you may not call any other methods of the class nor create any data structures such as arrays, lists, etc. Your method should not change the structure or contents of the tree.
-<br>Assume that you are adding this method to the IntTree class as defined below:
-<br>public class IntTree {
-<br>\tprivate IntTreeNode overallRoot;
-<br>\t...
-<br>}
 `
 
 class Workspace extends Component {
@@ -79,7 +105,7 @@ class Workspace extends Component {
           elementLeft={<Link to="/q">🔥</Link>}
         />
         <div style={{
-          padding: '5% 7%'
+          padding: '1% 7%'
         }}>
           <ProblemPrompt promptText={this.state.problemPrompt} />
           <div>
@@ -120,4 +146,27 @@ export default styled(Workspace)`
   background-color: ${backgroundColor};
   min-height: 100%;
   height: 100%;
+  
+  table {
+    width: 100%;
+    text-align: left;
+    
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 5px;
+  }
+  
+  thead {
+    color: ${primaryColor};
+  }
+  
+  tbody {
+    color: ${textColor};
+  }
+  
+  code {
+    background-color: grey;
+    border-radius: 5px;
+    padding: 3px;
+  }
 `
